4b12eb22521e39f25c9bb9da728035846ce90291
fenguopeng <fenguopeng@gmail.com>
2024-05-22 13:52:58 +0800

    fix pwn bug

--- Pwn/_files/flag.sh
+++ Pwn/_files/flag.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # 生成动态flag
 echo $FLAG > /home/ctf/flag
-chmod 740 /home/ctf/flag
+chmod 440 /home/ctf/flag
 
 # 删除环境变量FLAG，避免非预期解
 unset FLAG
--- Pwn/_files/start.sh
+++ Pwn/_files/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Add your startup script
 
 # 有自定义 FLAG 脚本则需要执行它，这里是从环境变量获取 FLAG，则需要 source 来执行这个脚本从而改写环境变量，避免费预期解
